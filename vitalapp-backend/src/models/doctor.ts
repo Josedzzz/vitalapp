5643c13da7b25769eade5090f415fc0387088c8f
Joki <josedavidamayar@gmail.com>
2025-04-07 15:00:14 -0500

    delete: delete the script to add doctors

--- vitalapp-backend/src/models/doctor.ts
+++ vitalapp-backend/src/models/doctor.ts
@@ -1,6 +1,5 @@
 import { ObjectId } from "npm:mongodb";
 import db from "../config/db.ts";
-import { hash } from "npm:bcryptjs";
 
 export interface DoctorSchema {
   _id?: ObjectId;
@@ -15,52 +14,3 @@ export const Doctors = db.collection<DoctorSchema>("doctors");
 
 // types for the doctor
 export type InsertDoctor = Omit<DoctorSchema, "_id"> & { _id: string };
-
-const sampleDoctors: Omit<DoctorSchema, "_id">[] = [
-  {
-    name: "Laura",
-    lastName: "González",
-    specialization: "General",
-    email: "laura.gonzalez@clinicapp.com",
-    password: "password123",
-  },
-  {
-    name: "Carlos",
-    lastName: "Ramírez",
-    specialization: "Cardiology",
-    email: "carlos.ramirez@clinicapp.com",
-    password: "password123",
-  },
-  {
-    name: "Sofía",
-    lastName: "Martínez",
-    specialization: "Psychology",
-    email: "sofia.martinez@clinicapp.com",
-    password: "password123",
-  },
-  {
-    name: "Andrés",
-    lastName: "Pérez",
-    specialization: "Pediatrics",
-    email: "andres.perez@clinicapp.com",
-    password: "password123",
-  },
-  {
-    name: "Valentina",
-    lastName: "Torres",
-    specialization: "Dermatology",
-    email: "valentina.torres@clinicapp.com",
-    password: "password123",
-  },
-];
-
-for (const doc of sampleDoctors) {
-  const hashedPassword = await hash(doc.password, 10); // 10 = salt rounds
-  const docToInsert = {
-    ...doc,
-    password: hashedPassword,
-  };
-  await Doctors.insertOne(docToInsert);
-}
-
-console.log("✅ Doctors inserted with hashed passwords.");
